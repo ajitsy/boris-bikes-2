4bc2830a1c3e64daf5c96f12e8c55d0b52ce4978
Winifred Au <winifredau1990@gmail.com>
2015-07-21 10:51:41 -0400

    added docking_station.rb and docking_station_spec.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
 require "docking_station"
 
 describe DockingStation do
-  
-end
+  it 'responds' do
+   expect(subject).to respond_to :release_bike
+   #it { is_expected.to respond_to :release_bike }
+   end
+ end
