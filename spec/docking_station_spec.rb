b768bd9e31912165b0a1ca0c8c60d704ee706dff
ajitsy <sanghera.p@gmail.com>
2015-07-21 19:02:12 +0100

    Changed docking_station & docking_station_spec for post challenge 1

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,13 +1,20 @@
 require "docking_station"
 
 describe DockingStation do
-  it 'responds' do
-   expect(subject).to respond_to :release_bike
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to :release_bike }
+
+  describe '#release_bike' do
+      it 'raises an error when there are no bikes available' do
+        expect { subject.release_bike }.to raise_error 'No bikes available'
+      end
     end
-   #it { is_expected.to respond_to :release_bike }
+
+    #it { is_expected.to respond_to :release_bike }
 
    it 'release bike and works' do
+     subject.dock Bike.new
      bike = subject.release_bike
      expect(bike).to respond_to :working?
-   end
+  end
  end
