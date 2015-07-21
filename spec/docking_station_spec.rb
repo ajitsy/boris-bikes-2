076a05176199b7d24745eb7a82a4b3f2f67ef2bd
Winifred Au <winifredau1990@gmail.com>
2015-07-21 12:15:52 -0400

    added changes to bike, docking_station, bike_spec and docking_station_spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,11 @@ require "docking_station"
 describe DockingStation do
   it 'responds' do
    expect(subject).to respond_to :release_bike
+    end
    #it { is_expected.to respond_to :release_bike }
+
+   it 'release bike and works' do
+     bike = subject.release_bike
+     expect(bike).to respond_to :working?
    end
  end
